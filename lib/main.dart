6aa5e3aa2949ae71d7349749fa5d72ea5c14e27f
KleberDevMen <klebersubcontas@gmail.com>
2020-04-30 11:49:02 -0300

    comentarios

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,41 @@
 import 'package:flutter/material.dart';
 
+// todo projeto inicia aqui, no main
 void main() {
+  // passando a tela home
   runApp(MaterialApp(title: 'Contado de pessoas', home: Home()));
 }
 
+// tela statica = state_less = nao possuem state (estado interno)
+// tela dinamica = state_full = possuem state (estado interno)
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
+// estado interno da minha tela home
 class _HomeState extends State<Home> {
+  // underline faz com que a variável só possa ser acessada aqui dentro do meu estado (privada)
   int _people = 0;
   String _infoText = "Pode entrar!";
 
+// underline == funcao privada
   void _changePeople(int delta) {
+    // executa o que esta dentro >> recarrega todo estado (refazendo apenas as partes modificadas)
     setState(() {
       _people += delta;
 
-      if(_people < 0){
+      if (_people < 0) {
         _infoText = 'Mundo invertido?!';
-      }else if(_people <= 10){
+      } else if (_people <= 10) {
         _infoText = 'Pode entrar!';
-      }else{
+      } else {
         _infoText = 'Lotado!';
       }
-
     });
   }
 
-  //  é chamada quando vamos modificar
+  // é chamada quando vamos modificar o layout
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -57,7 +64,8 @@ class _HomeState extends State<Home> {
                       style: TextStyle(fontSize: 40.0, color: Colors.white),
                     ),
                     onPressed: () {
-                     _changePeople(1);
+                      _changePeople(1);
+                      // debugPrint('+1');
                     },
                   ),
                 ),
